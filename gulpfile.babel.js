4a7281c288834831aaf66c3a8ffacc83192b5020
tsapli <githubchick@gmail.com>
2021-03-07 04:58:49 +0200

    test

---
+++ gulpfile.babel.js
@@ -0,0 +1,13 @@
+import config from './gulp/config';
+
+config.setEnv();
+
+export const build = () => {
+  console.log(config.isProd);
+};
+
+export const watching = () => {
+  console.log(config.isDev);
+};
+
+const test = 1
